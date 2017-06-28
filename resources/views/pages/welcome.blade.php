74fdd226618876e09a39acb1a85891ea42cac587
Hugo Kuipers <Hugokuipers@hotmail.com>
2017-06-29 00:06:40 +0200

    Zodat het hier ook werkt minor

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,7 +6,7 @@
       <div class="jumbotron">
         <h1>Welcome to My Blog!</h1>
         <p class="lead">Thank you for visiting, this is a test site to practice Laravel. Please read the popular post!</p>
-        <p><a class="btn btn-primary btn-lg" href="{{ url("blog/".$posts[0]->slug) }}" role="button">Popular Post</a></p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a></p>
       </div>
     </div>
   </div>
